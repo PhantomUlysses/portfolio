c81fbd591f4a2cb14c3ec177e91e2384c59471fd
Nikita Stasiulevich <nikitastas1694@gmail.com>
2022-03-17 00:10:58 +0300

    add second block

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import Header from "./header/Header";
-import Nav from "./nav/Nav";
+import Main from "./main/Main";
+import Skills from "./skills/Skills";
 
 function App() {
   return (
     <div className="App">
         <Header />
+        <Main />
+        <Skills />
     </div>
   );
 }
