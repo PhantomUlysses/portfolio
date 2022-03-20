6282164a7dba3a4ace7a86a409cb85c6c02ae61b
Nikita Stasiulevich <nikitastas1694@gmail.com>
2022-03-20 21:00:56 +0300

    add my skills in portfolio

--- src/skills/Skills.js
+++ src/skills/Skills.js
@@ -1,6 +1,7 @@
 import React from "react";
 import s from './Skills.module.css';
 import sContainer from '../common/styles/Container.module.css'
+import Skill from "./skill/Skill";
 
 function Skills() {
     return (
@@ -8,7 +9,9 @@ function Skills() {
             <div className={`${sContainer.container} ${s.skillsContainer}`}>
                 <h2 className={s.title}>Skills</h2>
                 <div className={s.skills}>
-
+                    <Skill title={"JS"} description={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, "}/>
+                    <Skill title={"CSS"} description={"sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."}/>
+                    <Skill title={"React"} description={"Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."}/>
                 </div>
             </div>
         </div>
